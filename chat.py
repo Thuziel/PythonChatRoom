cbfb5ab58ba8538dd28be466f1c754ff75de2d47
Seth Clemans <48653177+Thuziel@users.noreply.github.com>
2020-12-28 20:33:20 -0600

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,30 @@
+try:
+    user = open(r"C:\username.txt", "r+")
+    
+except:
+    import os 
+    path = 'C:\\'
+    file = 'username.txt'
+    username = input("Username: ")
+    with open(os.path.join(path, file), 'w') as fp: 
+        pass
+        fp.write(username)
+    print("Username successfully generated\n")
+
+    
+displayMsgs = open("messages.txt", "r+")
+appendMags =  open("messages.txt", "a+")
+    
+try:
+    for line in displayMsgs:
+        print(line)
+    msg = input("\nReply: ")
+    appendMags.write("\n"+user.read()+": "+msg)
+    appendMags.close()
+    print(displayMsgs.read())
+    print("Message successfully sent!")
+except:
+    print("\nMessage failed to send. Please report it here: sethuclemans@gmail.com\nThanks!")
+finally:
+    appendMags.close()
+    displayMsgs.close()
